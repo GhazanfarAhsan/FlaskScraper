1574f50ecf519f3fc4568720f890e9bffc120d90
Ghazanfar Ahsan <52095337+GhazanfarAhsan@users.noreply.github.com>
2022-11-12 19:00:10 +0500

    Update ScraperController.py

--- EliveScrapers/controllers/ScraperController.py
+++ EliveScrapers/controllers/ScraperController.py
@@ -3,7 +3,6 @@ from pickle import TRUE
 from flask import Flask
 import requests
 from bs4 import BeautifulSoup
-from db import database
 
 from controllers.BaseController import BaseController;
 from controllers.AmazonController import AmazonController;
@@ -12,15 +11,25 @@ class ScraperController(BaseController):
     def __init__(self):
         self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
         self.cookies = {};
+        self.baseUrl = "";
     
-    def scraper(self,data):
+    def setBaseUrl(self,baseUrl):
+        self.baseUrl = baseUrl;
+    
+    def getBaseUrl(self): 
+        return self.baseUrl;
+
+    def redirect(self,data):
             if(data['market'] =="amazon"):
                 amazon = AmazonController();
                 return amazon.scrapAmazonBestSellerProducts(data);
             
-
+    def setupDriver(self):
+        print("Setting up the driver")
+        # Set your driver here
     
-    
+    def makeRequest(self,url):
+        response = requests.get(self.baseUrl+url);
     
 
     
