578fe1a467ebb826399e87d72845b64f020b7896
Usama Raza <63732785+usamaraza21@users.noreply.github.com>
2022-11-12 20:25:34 +0500

    Adding selenium in scraperController
    >Add ChromeDriver in Assets Folder
    >made changes in setupDriver and makeRequest Methods.

--- EliveScrapers/controllers/ScraperController.py
+++ EliveScrapers/controllers/ScraperController.py
@@ -3,16 +3,19 @@ from pickle import TRUE
 from flask import Flask
 import requests
 from bs4 import BeautifulSoup
-
+from selenium import webdriver
+import os
 from controllers.BaseController import BaseController;
 from controllers.AmazonController import AmazonController;
 
 class ScraperController(BaseController):
+
+    
     def __init__(self):
         self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
         self.cookies = {};
         self.baseUrl = "";
-    
+        self.requestType = "selenium";   
     def setBaseUrl(self,baseUrl):
         self.baseUrl = baseUrl;
     
@@ -25,11 +28,23 @@ class ScraperController(BaseController):
                 return amazon.scrapAmazonBestSellerProducts(data);
             
     def setupDriver(self):
-        print("Setting up the driver")
         # Set your driver here
-    
+        print("Setting up the driver")
+        driverPath = os.path.abspath('chromedriver.exe') 
+        return driverPath
+
     def makeRequest(self,url):
-        response = requests.get(self.baseUrl+url);
+        if  self.requestType == "selenium":
+            driver = webdriver.Chrome(self.setupDriver())
+            response = driver.get(self.baseUrl+url)
+
+        elif self.requestType == "requests":
+            response = requests.get(self.baseUrl+url);
+        else:
+            response = "You have to decide response type"
+
+        return response
+            
     
 
     
